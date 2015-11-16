c287411087ac12dd145cd85aa610d124814b6337
Dmitrii Kuvaiskii <devnull@localhost>
2015-11-16 11:00:06 +0100

    added fault-injection flag

--- config/clang.mk
+++ config/clang.mk
@@ -3,4 +3,4 @@ include $(PHOENIXHOME)/config/gcc.mk
 CC = $(LLVMHOME)/bin/clang
 RANLIB = /bin/true
 
-CFLAGS += -mrtm -flto -Wl,-plugin-opt=save-temps -funroll-loops
+CFLAGS += -mrtm -flto -Wl,-plugin-opt=save-temps -funroll-loops #-DFAULTINJECTION
